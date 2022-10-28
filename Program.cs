fbdde99a159a7dd07aa0955c86c351fa2ebf458d
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-10-28 15:31:16 +0200

    pulisco il codice

--- Program.cs
+++ Program.cs
@@ -2,73 +2,18 @@
 using System;
 
 int[] arrayToPrint = { 1, 2, 3, 4, 5, 6 };
-
 StampaArray(arrayToPrint);
-    
-void StampaArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(i == array.Length - 1)   
-            Console.Write(array[i]);
-        else
-            Console.Write(array[i] + ",");
-
-    }
-    Console.Write(" ]");
-}
-
 Console.WriteLine();
 Console.WriteLine("inserisci un numero");
 int userValue = Convert.ToInt32(Console.ReadLine());
-
 int result = Quadrato(userValue);
-
-int Quadrato(int value)
-{
-    return value * value;
-}
-
 Console.WriteLine(result + " è il suo quadrato");
-
-   
 int[] print = ElevaArrayAlQuadrato(arrayToPrint);
-
-int[] ElevaArrayAlQuadrato(int[] array)
-{ 
-    int[] arrayToPrint = new int[array.Length];
-
-    for (int i = 0; i < array.Length; i++)
-    {
-
-        arrayToPrint[i] = Quadrato(array[i]);
-    }
-
-    return arrayToPrint;
-}
-
 StampaArray(print);
 Console.WriteLine();
- 
 int totalArraySum = SommaElementiArray(arrayToPrint);
-
-int SommaElementiArray(int[] array){
-
-    int total = 0;
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        total += array[i];
-    }
-
-    return total;
-}
-
 Console.WriteLine(totalArraySum);
-
 int[] numeri = { 2, 6, 7, 5, 3, 9 };
-
 AllFunction(numeri);
 bool loop = true;
 
@@ -111,4 +56,48 @@ void AllFunction(int[] numeri)
     Console.WriteLine(totalNumeriSum);
     int totalNumeriSumElevate = SommaElementiArray(printNumeriArray);
     Console.WriteLine(totalNumeriSumElevate);
+}
+
+int SommaElementiArray(int[] array){
+
+    int total = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        total += array[i];
+    }
+
+    return total;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{ 
+    int[] arrayToPrint = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        arrayToPrint[i] = Quadrato(array[i]);
+    }
+
+    return arrayToPrint;
+}
+
+int Quadrato(int value)
+{
+    return value * value;
+}
+
+void StampaArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == array.Length - 1)   
+            Console.Write(array[i]);
+        else
+            Console.Write(array[i] + ",");
+
+    }
+    Console.Write(" ]");
 }
